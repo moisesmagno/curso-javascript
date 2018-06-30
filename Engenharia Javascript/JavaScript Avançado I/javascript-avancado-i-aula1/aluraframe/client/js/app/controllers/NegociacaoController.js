ff3e1ae672f10721a29537b15ca964cda844fc13
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2018-06-30 00:46:54 -0300

    Criado a classe Helpers e efetuado melhorias no código como um todo.

--- Engenharia Javascript/JavaScript Avançado I/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Engenharia Javascript/JavaScript Avançado I/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -11,20 +11,16 @@ class NegociacaoController{
 
 
 	adiciona(event){
+
 		event.preventDefault();
 
-		let data = new Date(...
-						this._inputData.value
-									.split("-")
-									.map(function(item,indice){
-										return item - indice % 2;
-									})
-							);
-			console.log(data);
 		let negociacao = new Negociacao(
-				data,
+				DateHelper.textoParaData(this._inputData.value),
 				this._inputQuantidade.value,
 				this._inputValor.value,
-			);
+		);
+
+		console.log(negociacao.data);
+		console.log(DateHelper.dataParaTexto(negociacao.data));
 	}
 }
