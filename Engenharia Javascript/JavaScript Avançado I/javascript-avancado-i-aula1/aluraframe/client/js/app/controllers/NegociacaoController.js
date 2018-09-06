6270c9df0b5c7a5e6a91f48863f5e5921a36db84
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2018-09-06 18:19:01 -0300

    Criado as funcionalidades de limpar a lista de negociações.

--- Engenharia Javascript/JavaScript Avançado I/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Engenharia Javascript/JavaScript Avançado I/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,7 +7,9 @@ class NegociacaoController{
 		this._inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
-		this._listaNegociacoes = new ListaNegociacoes();
+		this._listaNegociacoes = new ListaNegociacoes(function(model){
+			this._negociacoesView.update(model);	
+		});
 
 		this._negociacoesView = new NegociacoesView($("#negociacoesView"));
 		this._negociacoesView.update(this._listaNegociacoes);
@@ -17,13 +19,17 @@ class NegociacaoController{
 		this._mensagemView.update(this._mensagem);
 	}
 
+	apaga(){
+		this._listaNegociacoes.esvazia();
+		this._mensagem.texto = "Negociações apagadas com sucesso!";
+		this._mensagemView.update(this._mensagem);
+	}
 
 	adiciona(event){
 
 		event.preventDefault();
 
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
-		this._negociacoesView.update(this._listaNegociacoes)
 		
 		this._mensagem.texto = "Negociação adicionada com sucesso!";
 		this._mensagemView.update(this._mensagem);
