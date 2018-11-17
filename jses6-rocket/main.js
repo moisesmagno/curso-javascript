866bb9535251d8ba1baeda91a67667f62221fbbf
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2018-11-16 22:00:38 -0200

    EFetuado estudos sobre classes, herança e métodos estáticos, no curso de ESÂ6 da Rocket.

--- jses6-rocket/main.js
+++ jses6-rocket/main.js
@@ -1,7 +1,51 @@
-alert('Hola Mundo!');
-
-class teste{
-    metodo(){
-        alert("Hola mundo!");
+class Matematica{
+    
+    // Um método estático, nunca encherga outros atributos e métodos da classe. 
+    static somar(n1, n2){
+        return n1 + n2;
     }
 }
+
+class List{
+    
+    constructor(element=[]){
+        this.data = element;
+    }
+
+    addData(data){
+        this.data.push(data);
+        console.log(this.data)
+    }
+
+}
+
+class TodoList extends List{
+
+    constructor(){
+        super();
+        this.todos = [];
+    }
+
+    addTodo(){
+        this.todos.push("Novo todo","List");
+        console.log(this.todos)
+    }
+}
+
+const minhaLista = new TodoList();
+
+let btn1 = document.querySelector("#btn_todoList");
+let btn2 = document.querySelector("#btn_novodado");
+let btn3 = document.querySelector("#btn_somar");
+
+btn1.addEventListener("click", function(){
+    minhaLista.addTodo();
+});
+
+btn2.addEventListener("click", function(){
+    minhaLista.addData("mensagem");
+})
+
+btn3.addEventListener("click", function(){
+    console.log(Matematica.somar(5,5));
+})
