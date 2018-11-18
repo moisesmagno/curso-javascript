f5e99515c3120099ce0141880d78ebcc53fef9cf
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2018-11-18 17:58:18 -0200

    Criado o projeto que consome uma api do github, onde foram usadas todos os conceitos aprendidos no curso ES6.

--- jses6-rocket/src/main.js
+++ jses6-rocket/src/main.js
@@ -1,16 +1,96 @@
-const minhaPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('OK')
-    },2000);
-});
+import api from './api'
 
-// minhaPromise.then(response => {
-//     console.log(response);
-// });
+class App{
+    
+    constructor(){
+        this.repositories = [];
+        // this.formEl = document.getElementById("repo-form");
+        this.formEl = document.querySelector("#repo-form");
+        this.listEl = document.querySelector("#repo-list");
+        this.inputEl = document.querySelector("input[name='username']");
+        this.registerHandlers();
+    }
 
-async function executaPromise(){
-    const response = await minhaPromise();
-    console.log(response);
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    setLoading(loading = true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id','loading');
+
+            this.formEl.appendChild(loadingEl)
+        }else{
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event){
+        event.preventDefault();
+        
+        const repoInput = this.inputEl.value
+
+        if(repoInput.lenght === 0)
+            return;
+        
+        this.setLoading();
+
+        try{
+
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const {name, description, html_url, owner:{avatar_url}} = response.data;
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url
+            });
+
+            this.inputEl.value = '';
+
+            this.render();
+
+        }catch(err){
+            
+            alert("O repositório não existe!");
+
+        }
+
+        this.setLoading(false);
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+    };
+
 }
 
-executaPromise();
+new App;
