ebc1c72be7869cac8feef5fab0ffc286111ce747
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2018-11-18 01:59:42 -0200

    Realizado diversos estudos no curso de ESS6 do rocket

---
+++ jses6-rocket/src/main.js
@@ -0,0 +1,16 @@
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('OK')
+    },2000);
+});
+
+// minhaPromise.then(response => {
+//     console.log(response);
+// });
+
+async function executaPromise(){
+    const response = await minhaPromise();
+    console.log(response);
+}
+
+executaPromise();
